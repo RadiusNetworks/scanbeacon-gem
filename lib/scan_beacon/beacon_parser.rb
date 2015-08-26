88b0bdaaba634b2eecde0a1cd39eb6d8aca5f4c1
Scott Yoder <yodersb@gmail.com>
2015-08-26 14:18:12 -0400

    add a BeaconId class that allows accessing as int or hex

--- lib/scan_beacon/beacon_parser.rb
+++ lib/scan_beacon/beacon_parser.rb
@@ -6,7 +6,7 @@ module ScanBeacon
     }
     AD_TYPE_MFG = 0xff
     AD_TYPE_SERVICE = 0x03
-    BT_EIR_SERVICE_DATA = "\x16"
+    BT_EIR_SERVICE_DATA = "\x16".force_encoding("ASCII-8BIT")
     attr_accessor :beacon_type
 
     def self.default_parsers
@@ -65,26 +65,13 @@ module ScanBeacon
     end
 
     def parse_data_fields(data)
-      parse_elems(@data_fields, data)
+      parse_elems(@data_fields, data).map(&:bytes)
     end
 
     def parse_elems(elems, data)
       elems.map {|elem|
         elem_str = data[elem[:start]..elem[:end]]
-        elem_length = elem_str.size
-        case elem_length
-        when 1
-          elem_str.unpack('C')[0]
-        when 2 
-          # two bytes, so treat it as a short (big endian)
-          elem_str.unpack('S>')[0]
-        when 6
-          # 6 bytes, treat it is an eddystone instance id
-          ("\x00\x00"+elem_str).unpack('Q>')[0]
-        else
-          # not two bytes, so treat it as a hex string
-          elem_str.unpack('H*').join
-        end
+        BeaconId.new(bytes: elem_str)
       }
     end
 
@@ -99,38 +86,30 @@ module ScanBeacon
 
     def generate_ad(beacon)
       length = [@matchers, @ids, @power, @data_fields].flatten.map {|elem| elem[:end] }.max + 1
-      ad = "\x00" * length
+      ad = ("\x00" * length).force_encoding("ASCII-8BIT")
       @matchers.each do |matcher|
         ad[matcher[:start]..matcher[:end]] = [matcher[:expected]].pack("H*")
       end
       @ids.each_with_index do |id, index|
-        ad[id[:start]..id[:end]] = generate_field(id, beacon.ids[index])
+        id_length = id[:end] - id[:start] + 1
+        id_bytes = BeaconId.id_with_length(beacon.ids[index], id_length).bytes
+        ad[id[:start]..id[:end]] = id_bytes
       end
       @data_fields.each_with_index do |field, index|
-        ad[field[:start]..field[:end]] = generate_field(field, beacon.data[index]) unless beacon.data[index].nil?
+        unless beacon.data[index].nil?
+          field_length = field[:end] - field[:start] + 1
+          field_bytes = BeaconId.id_with_length(beacon.data[index], field_length).bytes
+          ad[field[:start]..field[:end]] = field_bytes
+        end
       end
       ad[@power[:start]..@power[:end]] = [beacon.power].pack('c')
       if @ad_type == AD_TYPE_SERVICE
-        "\x03\x03" + [beacon.service_uuid].pack("S<") + [length+1].pack('C') + BT_EIR_SERVICE_DATA + ad
+        "\x03\x03".force_encoding("ASCII-8BIT") + [beacon.service_uuid].pack("S<") + [length+1].pack('C') + BT_EIR_SERVICE_DATA + ad
       elsif @ad_type == AD_TYPE_MFG
         ad[0..1] = [beacon.mfg_id].pack("S<")
         [length+1].pack('C') + [AD_TYPE_MFG].pack('C') +  ad
       end
     end
-
-    def generate_field(field, value)
-      field_length = field[:end] - field[:start] + 1
-      case field_length
-      when 1
-        [value].pack("c")
-      when 2
-        [value].pack("S>")
-      when 6
-        [value].pack("Q>")[2..-1]
-      else
-        [value].pack("H*")[0..field_length-1]
-      end
-    end 
 
     def inspect
       "<BeaconParser type=\"#{@beacon_type}\", layout=\"#{@layout.join(",")}\">"
