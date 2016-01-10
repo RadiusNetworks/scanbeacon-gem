b7740eee89532e29ed7e2286ec042c3fa6b24083
Scott Yoder <yodersb@gmail.com>
2016-01-10 15:36:18 -0500

    slight refactor of compress & decompress code

--- lib/scan_beacon/eddystone_url_beacon.rb
+++ lib/scan_beacon/eddystone_url_beacon.rb
@@ -2,8 +2,6 @@ module ScanBeacon
   # Convenience class for constructing & advertising Eddystone-URL frames
   class EddystoneUrlBeacon < Beacon
 
-    attr_accessor :url
-
     SCHEMES = {"http://www."  => "\x00",
                "https://www." => "\x01",
                "http://"      => "\x02",
@@ -25,33 +23,50 @@ module ScanBeacon
                   ".gov"   => "\x0d"}
 
     def initialize(opts = {})
-      @url = opts[:url]
-      opts[:ids] = [compressed_url_hex]
       opts[:service_uuid] ||= 0xFEAA
       opts[:beacon_type] ||= :eddystone_url
       super opts
+      self.url = opts[:url] if opts[:url]
+    end
+
+    def self.from_beacon(beacon)
+      new(ids: beacon.ids, power: beacon.power, rssis: beacon.rssis)
+    end
+
+    def url
+      @url ||= self.class.decompress_url( self.ids[0].to_s )
     end
 
     def url=(new_url)
       @url = new_url
-      self.ids = [compressed_url_hex]
+      self.ids = [self.class.compress_url(@url)]
     end
 
-    def compressed_url
+    def self.compress_url(url)
       scheme, scheme_code = SCHEMES.find {|k, v| url.start_with? k}
-      compressed_url = scheme_code + self.url[scheme.size..-1]
+      raise ArgumentError, "Invalid URL" if scheme.nil?
+      compressed_url = scheme_code + url[scheme.size..-1]
       EXPANSIONS.each do |k,v|
         compressed_url.gsub! k,v
       end
-      return compressed_url.force_encoding("ASCII-8BIT")
+      raise ArgumentError, "URL too long" if compressed_url.size > 18
+      compressed_url.force_encoding("ASCII-8BIT").unpack("H*")[0]
     end
 
-    def compressed_url_hex
-      self.compressed_url.unpack("H*")[0]
+    def self.decompress_url(hex)
+      compressed_url_string = [hex].pack("H*")
+      scheme_code = compressed_url_string[0]
+      scheme, scheme_code = SCHEMES.find {|k,v| v == scheme_code}
+      raise ArgumentError, "Invalid URL" if scheme.nil?
+      decompressed_url = scheme + compressed_url_string[1..-1]
+      EXPANSIONS.each do |k,v|
+        decompressed_url.gsub! v,k
+      end
+      decompressed_url
     end
 
     def inspect
-      "<EddystoneUrlBeacon url=#{@ids.join(",")} rssi=#{rssi}, scans=#{ad_count}, power=#{@power}>"
+      "<EddystoneUrlBeacon url=\"#{url}\" rssi=#{rssi}, scans=#{ad_count}, power=#{@power}>"
     end
   end
 end
--- spec/eddystone_url_beacon_spec.rb
+++ spec/eddystone_url_beacon_spec.rb
@@ -4,14 +4,36 @@ require 'scan_beacon'
 module ScanBeacon
   RSpec.describe EddystoneUrlBeacon do
     let(:url) { "http://www.radiusnetworks.com" }
-    let(:beacon) { EddystoneUrlBeacon.new(url: url) }
+    let(:compressed_url) { "007261646975736e6574776f726b7307" }
 
     it "can compress a url according to the Eddystone-URL spec" do
-      expect(beacon.compressed_url).to eq "\x00radiusnetworks\a"
+      expect(EddystoneUrlBeacon.compress_url(url)).to eq compressed_url
     end
 
-    it "can be used to create a beacon object with the proper id field" do
-      expect(beacon.ids).to eq ["007261646975736e6574776f726b7307"]
+    it "can decompress an url according to the Eddystone-URL spec" do
+      expect(EddystoneUrlBeacon.decompress_url(compressed_url)).to eq url
+    end
+
+    it "can be initialized with a url parameter and produce the proper ids" do
+      beacon = EddystoneUrlBeacon.new(url: url)
+      expect(beacon.ids).to eq [compressed_url]
+    end
+
+    it "can be initialized with a hex id parameter and return the decompressed url" do
+      beacon = EddystoneUrlBeacon.new(ids: [compressed_url])
+      expect(beacon.url).to eq url
+    end
+
+    it "should raise InvalidArgument when given an invalid url to compress" do
+      expect{ EddystoneUrlBeacon.compress_url "foobar" }.to raise_error ArgumentError
+    end
+
+    it "should raise InvalidArgument when given a url to compress that is too long" do
+      expect{ EddystoneUrlBeacon.compress_url "http://google.com/url_is_too_long" }.to raise_error ArgumentError
+    end
+
+    it "should raise InvalidArgument when a url can't be decompressed" do
+      expect{ EddystoneUrlBeacon.decompress_url "garbage" }.to raise_error ArgumentError
     end
   end
 end
