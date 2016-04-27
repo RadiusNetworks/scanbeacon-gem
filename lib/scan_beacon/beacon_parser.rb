8da479c0c03cbf7b1d90d45bc8a7734cabd38d46
Scott Yoder <yodersb@gmail.com>
2016-04-27 10:20:24 -0400

    fix typo in UID layout; fixes #25;

--- lib/scan_beacon/beacon_parser.rb
+++ lib/scan_beacon/beacon_parser.rb
@@ -2,7 +2,7 @@ module ScanBeacon
   class BeaconParser
     DEFAULT_LAYOUTS = {
       altbeacon: "m:2-3=beac,i:4-19,i:20-21,i:22-23,p:24-24,d:25-25",
-      eddystone_uid: "s:0-1=feaa,m:2-2=00,p:3-3:-41,i:4-13,i:14-19;d:20-21",
+      eddystone_uid: "s:0-1=feaa,m:2-2=00,p:3-3:-41,i:4-13,i:14-19,d:20-21",
       eddystone_url: "s:0-1=feaa,m:2-2=10,p:3-3:-41,i:4-21v",
       eddystone_tlm: "s:0-1=feaa,m:2-2=20,d:3-3,d:4-5,d:6-7,d:8-11,d:12-15",
       eddystone_eid: "s:0-1=feaa,m:2-2=30,p:3-3:-41,i:4-11",
