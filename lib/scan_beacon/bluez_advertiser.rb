2e804cee685763d0fc4b340962731bae688201c8
Aaron Kromer <aaron.kromer@gmail.com>
2016-05-17 18:02:29 -0400

    Fix assigned but unused variable warnings
    > /lib/scan_beacon/ble112_device.rb:137: warning: assigned but unused variable - device_count
    > /lib/scan_beacon/bluez_advertiser.rb:10: warning: assigned but unused variable - addr

--- lib/scan_beacon/bluez_advertiser.rb
+++ lib/scan_beacon/bluez_advertiser.rb
@@ -7,7 +7,7 @@ module ScanBeacon
       @device_id = opts[:device_id] || BlueZ.devices.map {|d| d[:device_id]}[0]
       raise "No available devices" if @device_id.nil?
       BlueZ.device_up @device_id
-      addr = @initial_addr = BlueZ.devices.find {|d| d[:device_id] == @device_id}[:addr]
+      @addr = @initial_addr = BlueZ.devices.find {|d| d[:device_id] == @device_id}[:addr]
       super(opts)
     end
 
